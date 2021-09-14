a2b6d9ff95cd4c15d33e9c1eee24056e12ed161e
lwise <lauren.wise@shopify.com>
2021-09-14 16:08:29 -0400

    Initial Commit

---
+++ TestFrameworkBugsnagRN/TestFrameworkBugsnagRN.h
@@ -0,0 +1,18 @@
+//
+//  TestFrameworkBugsnagRN.h
+//  TestFrameworkBugsnagRN
+//
+//  Created by Lauren Wise on 2021-09-14.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestFrameworkBugsnagRN.
+FOUNDATION_EXPORT double TestFrameworkBugsnagRNVersionNumber;
+
+//! Project version string for TestFrameworkBugsnagRN.
+FOUNDATION_EXPORT const unsigned char TestFrameworkBugsnagRNVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestFrameworkBugsnagRN/PublicHeader.h>
+
+
